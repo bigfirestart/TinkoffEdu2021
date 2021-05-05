ca093e98848ad1c47625cf21ba44c913fd3668b9
Mark <bigfirestart@gmail.com>
2021-05-05 15:09:39 +0300

    feat: tests inited

---
+++ TinkoffEduUITests/TinkoffEduUITests.swift
@@ -0,0 +1,14 @@
+//
+//  TinkoffEduUITests.swift
+//  TinkoffEduUITests
+//
+//  Created by Кирилл Лукьянов on 05.05.2021.
+//
+
+import XCTest
+
+class TinkoffEduUITests: XCTestCase {
+    func testExample() throws {
+        
+    }
+}
---
+++ TinkoffEduUnitTests/TinkoffEduUnitTests.swift
@@ -0,0 +1,14 @@
+//
+//  TinkoffEduUnitTests.swift
+//  TinkoffEduUnitTests
+//
+//  Created by Кирилл Лукьянов on 05.05.2021.
+//
+
+import XCTest
+
+class TinkoffEduUnitTests: XCTestCase {
+    func testExample() throws {
+            XCTAssertEqual(1, 1)
+    }
+}
