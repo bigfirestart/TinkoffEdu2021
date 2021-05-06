f77a718d2f5df57c67fce7a515f0922f40fa1def
Mark <bigfirestart@gmail.com>
2021-05-06 13:40:02 +0300

    fix: new rbbi segue to custom

--- TinkoffEduUITests/TinkoffEduUITests.swift
+++ TinkoffEduUITests/TinkoffEduUITests.swift
@@ -8,15 +8,18 @@
 import XCTest
 
 class TinkoffEduUITests: XCTestCase {
-    func testExample() throws {
+        func testProfileTextFields() throws {
+        // Arrang
         let app = XCUIApplication()
+            
+        // Act
         app.launch()
-        
         app.navigationBars["Tinkoff Chat"].buttons["Profile"].tap()
         
         let textFields = app.textFields
         let textViews = app.textViews
         
+        // Assert
         XCTAssertEqual(textFields.count, 1)
         XCTAssertEqual(textViews.count, 1)
        
